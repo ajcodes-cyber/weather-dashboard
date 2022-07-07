5c2d85f8d3bbde1b0a8bb5a894931a853481c53e
ajcodes-cyber <anthonyjohn350@gmail.com>
2022-07-07 06:53:07 -0500

    adding the min and max for loops

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,4 +4,13 @@ function getInfo() {
     cityName.innerHTML = "--"+newName.value+"--";
 }
 
-fetch("http://api.openweathermap.org/data/2.5/forecast?q=dallas&appid=efdd3491500955b39f293ff6b769d949")
+fetch("http://api.openweathermap.org/data/2.5/forecast?q='+newName.value+'&appid=efdd3491500955b39f293ff6b769d949")
+.then(response => response.json())
+.then(data =>{
+    for(i = 0; i < 5; i++) {
+        document.getElementById("day" +(i+1)+"Min").innerHTML = "Min:" +Number(data.list[i].main.temp_min -300.52).toFixed(1)+"°";
+    }
+    for(i = 0; i < 5; i++) {
+        document.getElementById("day" +(i+1)+"Max").innerHTML = "Max:" +Number(data.list[i].main.temp_max -300.52).toFixed(1)+"°";
+    }
+})
